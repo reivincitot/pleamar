7bc00183073aed031a3b82f4dc94d96418577313
Sebastian <reivincitot@gmail.com>
2024-07-23 02:38:06 -0400

    Creación de los primeros modelos de usuarios

--- users_app/models.py
+++ users_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=20, null=False, blank=False, verbose_name="Nombre")
+    last_name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Apellidos')
+    birth_day = models.DateField(null=False, blank=False, verbose_name='Fecha de nacimiento')
+    address = models.CharField(null=True, blank=True, verbose_name='Dirección')
+    phone_number = models.CharField(max_length=12, blank=False, null=False, verbose_name='Número de teléfono')
+    group = models.ForeignKey('auth.Group',verbose_name='Puesto', on_delete=models.SET_DEFAULT,default=1)
+    email = models.EmailField(unique=True, null=False, blank=False, verbose_name='Correo electrónico')
+
+    class Meta:
+        verbose_name = 'Usuario personalizado'
+        verbose_name_plural = 'Usuarios personalizados'
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        related_name='customuser_set',
+        related_query_name='customuser'
+    )
+
+    user_permissions= models.ManyToManyField(
+        'auth.Permission',
+        verbose_name= 'user permission',
+        blank=True,
+        related_name='customuser_set',
+        related_query_name='customuser'
+    )
--- users_app/views.py
+++ users_app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        username= request.POST.get('username')
+        password= request.POST.GET('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home') # redirige a la pagina home luego de haber iniciado sesión
+        else:
+            messages.error(request,'Credenciales inválidas. Inténtelo de nuevo')
+    else:
+      return render(request,'account/login.html')
